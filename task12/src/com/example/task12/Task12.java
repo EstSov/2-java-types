0f73dc03f2063046cdf5eb61066ded05e9f01eff
EstSov <fedorovama@gmail.com>
2021-09-21 18:38:27 +0500

    task12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -9,15 +9,12 @@ public class Task12 {
         // TODO раскомментируйте и исправьте код
 
         // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
 
-        return BigDecimal.ZERO;
+        //return BigDecimal.ZERO;
     }
 
     public static void main(String[] args) {
