d6dae6c4cb94a76f96b15a52d8d29d16b3f5427b
EstSov <fedorovama@gmail.com>
2021-09-21 13:35:32 +0500

    task07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -5,17 +5,29 @@ public class Task07 {
     public static int solution(int n, int m, int k) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int tentArea = k*k;
+        int tentsArea = 0;
+        int count =0;
+        int kInN=n/k;
+        int kInM=m/k;
+        for(int i=0;i<kInN;i++)
+        {
+            for(int j=0;j<kInM;j++)
+            {
+                count +=1;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+
+        int result = solution(1000000, 1000000, 50);
         System.out.println(result);
-        */
+
+
     }
 
 }
